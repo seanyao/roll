018cea685985098376d5057239f9ada567fe1a09
Sean Yao <sean.dlut@gmail.com>
2026-06-02 03:51:23 +0800

    tcr: add roll changelog generate (US-CL-006)
    Co-Authored-By: Kimi <kimi@agent>

--- lib/i18n/changelog.sh
+++ lib/i18n/changelog.sh
@@ -1,3 +1,5 @@
 
 _i18n_set en changelog.unknown_subcommand "Unknown subcommand: %s"
 _i18n_set zh changelog.unknown_subcommand "未知子命令：%s"
+_i18n_set en changelog.generate_help "Generate draft ## Unreleased entries from backlog ✅ Done stories"
+_i18n_set zh changelog.generate_help "从 backlog 已完成的 Story 生成 ## Unreleased 草稿"
